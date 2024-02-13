bed4510664aae31fc47d7bfb078a700479f1cdfc
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-02-13 08:51:20 +0000

    Update site documentation

---
+++ docs/trait.impl/serde/de/trait.Deserialize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"lambda":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.188/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"lambda/struct.S3Details.html\" title=\"struct lambda::S3Details\">S3Details</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.188/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"lambda/struct.MessageBody.html\" title=\"struct lambda::MessageBody\">MessageBody</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
